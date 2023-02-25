be8cb351097064b54d9828781614dd7c27d46155
丁少华 <960423114@qq.com>
2023-02-25 16:55:35 +0800

    feat: init

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import {
+    Mark,
+    mergeAttributes,
+} from "@tiptap/core";
+import './style.css';
+
+
+
+export default Mark.create({
+    name: "dot",
+    addAttributes() {
+        return {
+            class: {
+                default: 'dot'
+            },
+            'data-type':{
+                default: 'dot'
+            }
+        }
+    },
+    parseHTML() {
+        return [
+            {
+                tag: 'span',
+                getAttrs: element => {
+                    return element.getAttribute('data-type') === 'dot';
+                },
+            },
+        ];
+    },
+    renderHTML({ HTMLAttributes }) {
+        return [
+            "span",
+            mergeAttributes(this.options.HTMLAttributes, HTMLAttributes),
+            0,
+        ];
+    },
+
+    addCommands() {
+        return {
+            setDot: () => ({ commands }) => {
+                return commands.setMark(this.name);
+            },
+            toggleDot: () => ({ commands }) => {
+                return commands.toggleMark(this.name);
+            },
+            unsetDot: () => ({ commands }) => {
+                return commands.unsetMark(this.name);
+            },
+        };
+    }
+});
